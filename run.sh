0662ca65b098597da61a08e5c174d85d17e88bc8
First <firstnaruto@msn.com>
2019-08-05 12:24:55 +0700

    [✨] Add run shell

---
+++ run.sh
@@ -0,0 +1,9 @@
+echo '### Install ZSH ###'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo '### Install Prezto ###'
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "~/${ZDOTDIR:-$HOME}/.zprezto"
+echo '### Copy File ###'
+cp .bash_profile ~/.bash_profile
+cp .zpreztorc ~/.zpreztorc
+cp .zshrc ~/.zshrc
+echo '### Finish ###'
